61237461a4cae4125680376b0279ed385e498cbb
Ryan Kliemann <Ryan_blake@icloud.com>
2015-11-09 14:58:42 -0800

    spam assignment partially done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,11 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+
+    @posts.first.title = "spam"
+    @posts.first.save!
+
+    # post.first.update_attributes(title: "spam")
   end
 
   def show
