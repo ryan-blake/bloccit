29fdf8b9a1e3e0bafbabaefc8717a9a7488c469a
Ryan Kliemann <Ryan_blake@icloud.com>
2015-11-10 14:29:02 -0800

    checkpoint 33 complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,19 @@
 class PostsController < ApplicationController
+
+
+def destroy
+  @post = Post.find(params[:id])
+
+  if @post.destroy
+    flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+    redirect_to posts_path
+  else
+    flash[:error] = "There was an error deleting the post."
+    render :show
+  end
+end
+
+
   def index
     @posts = Post.all
 
@@ -26,12 +41,28 @@ class PostsController < ApplicationController
       redirect_to @post
     else
 
-      flash[:error] = "There was an erro saving the post. Please try again."
+      flash[:error] = "There was an error saving the post. Please try again."
       render :new
     end
   end
 
 
   def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    if @post.save
+      flash[:notice] = "Post was updated"
+      redirect_to @post
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :edit
+    end
+
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,12 @@ ActiveRecord::Schema.define(version: 20151107161811) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "body"
-    t.integer  "Question_id"
+    t.integer  "question_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
 
-  add_index "answers", ["Question_id"], name: "index_answers_on_Question_id"
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id"
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
