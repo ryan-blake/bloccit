2252509002ae3239cfb9a645a3188441523dcb83
Ryan Kliemann <Ryan_blake@icloud.com>
2015-11-11 10:45:31 -0800

    33

--- db/migrate/20151111004910_add_topic_to_posts.rb
+++ db/migrate/20151111004910_add_topic_to_posts.rb
@@ -4,3 +4,6 @@ class AddTopicToPosts < ActiveRecord::Migration
     add_index :posts, :topic_id
   end
 end
+
+
+#"Add" + [table whose id we to add] + "To" + [table we want to add the foreign key to]
