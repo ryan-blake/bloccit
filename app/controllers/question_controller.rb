546b673dc0e0a88c8bd1b20255e52c6ea1f9ad0f
Ryan Kliemann <Ryan_blake@icloud.com>
2015-11-11 11:27:23 -0800

    check_33_assignment_#5

---
+++ app/controllers/question_controller.rb
@@ -0,0 +1,29 @@
+class QuestionController < ApplicationController
+  def new
+    @question = question.new
+  end
+
+  def create
+    @question = question.
+  end
+
+  def update
+    @question = question.
+  end
+
+  def edit
+    @question = question.
+  end
+
+  def destroy
+    @question = question.
+  end
+
+  def index
+    @question = question.all
+  end
+
+  def show
+    @question = question.
+  end
+end
