40e5922c2ed4b0a15a55c3a38f63c5b22055ca35
Ryan Kliemann <Ryan_blake@icloud.com>
2015-11-10 16:59:02 -0800

    at rake db:migrate

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
+include RandomData
 
 RSpec.describe Comment, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
   let(:post) { Post.create!(title: "New Post Title", body: "New Post Body")}
+  let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)}
+  let(:post) { Topic.post.create!(title: RandomData.random_sentence, description: RandomData.random_paragraph)}
   let(:comment) { Comment.create!(body: 'Comment Body', post: post)}
 
   describe "attributes" do
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,22 +1,26 @@
 require 'rails_helper'
+include RandomData
 
 # The migration is written in Ruby. The migration file is actually a class named CreatePosts. When we run the migration, the change method calls the create_table method. create_table takes block which specifies the attributes we want our table to possess.
 
 RSpec.describe Post, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 
+  let(:post) { Post.create!(title: "New Post Title", body: "New Post Body")}
 
-let(:post) { Post.create!(title: "New Post Title", body: "New Post Body")}
+  let (:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)}
 
-describe "Attributes" do
+  let (:post) { Topic.posts.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)}
 
-  it "should respond to title" do
-    expect(post).to respond_to(:title)
+  it { should belong_to(:topic) }
+  describe "Attributes" do
+
+    it "should respond to title" do
+      expect(post).to respond_to(:title)
+    end
+
+    it "should respond to body" do
+      expect(post).to respond_to(:body)
+    end
   end
-
-  it "should respond to body" do
-    expect(post).to respond_to(:body)
-  end
-end
-
 end
