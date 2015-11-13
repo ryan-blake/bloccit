0e242ee614fb51c6a7a9f531b4d88de9af5d6ded
Ryan Kliemann <Ryan_blake@icloud.com>
2015-11-12 16:06:37 -0800

    checkpoint35

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,12 +3,18 @@ include RandomData
 # The migration is written in Ruby. The migration file is actually a class named CreatePosts. When we run the migration, the change method calls the create_table method. create_table takes block which specifies the attributes we want our table to possess.
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
 # #4
   let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
 
   it { should belong_to(:topic) }
+
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:body) }
+  it { should validate_presence_of(:topic) }
+# 2
+  it { should validate_length_of(:title).is_at_least(5) }
+  it { should validate_length_of(:body).is_at_least(20) }
 
   describe "attributes" do
     it "should respond to title" do
