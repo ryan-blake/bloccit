59728838f9941bb420aee5458d086b75f9d8ee19
Ryan Kliemann <Ryan_blake@icloud.com>
2015-11-13 09:52:21 -0800

    assignment _35

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,10 @@
 class Topic < ActiveRecord::Base
   has_many :posts
+
+it { should validate_presence_of(:title) }
+it { should validate_presence_of(:body) }
+
+it { should validate_length_of(:title).is_at_least(5) }
+it { should validate_length_of(:body).is_at_least(15) }
+
 end
