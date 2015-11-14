10d94ffa3a5f1d3bb57a180f049a7c5fb5008dab
Ryan Kliemann <Ryan_blake@icloud.com>
2015-11-13 19:34:56 -0800

    @@

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,11 @@
 Rails.application.routes.draw do
-  get 'posts/index'
 
-  get 'posts/show'
-
-  get 'posts/new'
-
-  get 'posts/edit'
 
   resources :posts
 
   get  'about' => 'welcome#about'
 
-  get 'welcome/contact'
 
-  get 'welcome/faq'
 
   root to: 'welcome#index'
 end
