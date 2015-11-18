0efe0dcd3bcf5f09af346f816f50d0c43de1ac0f
Ryan Kliemann <Ryan_blake@icloud.com>
2015-11-17 17:59:05 -0800

    fixed bugs from previous checkpoints

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,5 +1,5 @@
 module CommentsHelper
   def user_is_authorized_for_comment?(comment)
-    current_user && (current_user == commment.user || current_user.admin?)
+     current_user && (current_user == comment.user || current_user.admin?)
   end
 end
