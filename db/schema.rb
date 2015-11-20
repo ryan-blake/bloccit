4b52fa631e623ffc3e5d65e5ce0bd9e26a58df8f
Ryan Kliemann <Ryan_blake@icloud.com>
2015-11-19 22:43:35 -0800

    wrong # of args

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151118051723) do
+ActiveRecord::Schema.define(version: 20151120053404) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "body"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20151118051723) do
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
     t.integer  "user_id"
+    t.float    "rank"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
@@ -86,5 +87,16 @@ ActiveRecord::Schema.define(version: 20151118051723) do
     t.datetime "updated_at",      null: false
     t.integer  "role"
   end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "value"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "votes", ["post_id"], name: "index_votes_on_post_id"
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id"
 
 end
