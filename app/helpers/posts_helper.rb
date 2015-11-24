493eac55e69ff4a0c0b18abd1c738e4a56a94f4e
Ryan Kliemann <Ryan_blake@icloud.com>
2015-11-24 14:23:38 -0600

    assign-45.notdone

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,8 @@ module PostsHelper
   def user_is_authorized_for_post?(post)
        current_user && (current_user == post.user || current_user.admin?)
   end
+
+  def user_has_no_post(user)
+    puts "#{user.name} has no posts yet"
+  end
 end
